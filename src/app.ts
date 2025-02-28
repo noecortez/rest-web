5b98f674337ec8eef86a5412eba5cc3881e8b63a
Noe Cortez <noe.martinez@goes.gob.sv>
2025-02-27 23:37:38 -0600

    feat(server): serve HTML file for root path and handle 404 errors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from 'fs'
 import http from 'http'
 
 const PORT = 8080
@@ -5,9 +6,15 @@ const PORT = 8080
 const server = http.createServer((request, response) => {
   console.log('🚀 ~ server ~ request.url:', request.url)
 
-  const data = { name: 'John Doe', age: 30, city: 'New York' }
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(data))
+  if (request.url === '/') {
+    const htmlFile = readFileSync('./public/index.html', 'utf-8')
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+
+    response.end(htmlFile)
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' })
+    response.end('<h1>404</h1>')
+  }
 })
 
 server.listen(PORT, () => {

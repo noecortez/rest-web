35f9d430d6d3e2101e6d75c33e7301668f5106ab
Noe Cortez <corteznoe3@gmail.com>
2025-02-28 14:49:33 -0600

    feat(server): serve JavaScript and CSS files with appropriate content types

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,17 @@ const server = http.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/html' })
 
     response.end(htmlFile)
-  } else {
-    response.writeHead(404, { 'Content-Type': 'text/html' })
-    response.end('<h1>404</h1>')
+    return
   }
+
+  if (request.url?.endsWith('.js')) {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' })
+  } else if (request.url?.endsWith('.css')) {
+    response.writeHead(200, { 'Content-Type': 'text/css' })
+  }
+
+  const responseContent = readFileSync(`./public${request.url}`, 'utf-8')
+  response.end(responseContent)
 })
 
 server.listen(PORT, () => {
